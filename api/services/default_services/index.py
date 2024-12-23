389c33810364f808bf3a9bca84c9a4d759411e48
rbardaji <rbardaji@gmail.com>
2024-12-23 11:19:05 +0000

    feat: Pass Kafka settings to index template

--- api/services/default_services/index.py
+++ api/services/default_services/index.py
@@ -1,9 +1,11 @@
+# api/services/default_services/index.py
+
 from fastapi import Request
 from fastapi.templating import Jinja2Templates
 
 from api.config import swagger_settings
+from api.config.kafka_settings import kafka_settings
 from api.services import status_services
-
 from api.services.keycloak_services.introspect_user_token import \
     get_client_token
 
@@ -35,6 +37,13 @@ def index(request: Request):
     except Exception:
         status = "CKAN and Keycloak are not reachable."
 
+    # Add Kafka information
+    kafka_info = {
+        "kafka_host": kafka_settings.kafka_host,
+        "kafka_port": kafka_settings.kafka_port,
+        "kafka_connection": kafka_settings.kafka_connection,
+    }
+
     return templates.TemplateResponse(
         request=request,
         name="index.html",
@@ -42,6 +51,7 @@ def index(request: Request):
             "title": swagger_settings.swagger_title,
             "version": swagger_settings.swagger_version,
             "docs_url": f"{request.base_url}docs",
-            "status": status
+            "status": status,
+            "kafka_info": kafka_info
         }
     )
