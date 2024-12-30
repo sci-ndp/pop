b9b0deb02bf4937f16403ca7b96adbf6914cbbbf
rbardaji <rbardaji@gmail.com>
2024-12-30 14:40:00 +0000

    chore: Update default value for jupyter_url

--- api/config/swagger_settings.py
+++ api/config/swagger_settings.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     swagger_version: str = "0.3.0"
     public: bool = True
     use_jupyterlab: bool = False
-    jupyter_url: str = "http://localhost:8888"
+    jupyter_url: str = "https://jupyter.org/try-jupyter/lab/"
 
     model_config = {
         "env_file": "./env_variables/.env_swagger",
