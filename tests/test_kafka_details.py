3127068f149fb73075de8a661012668a2299a87a
rbardaji <rbardaji@gmail.com>
2025-01-13 13:22:23 +0000

    test: update test to include kafka_prefix and max_streams

--- tests/test_kafka_details.py
+++ tests/test_kafka_details.py
@@ -9,7 +9,9 @@ def test_kafka_details_route():
     # Mock the individual attributes of kafka_settings
     with patch('api.config.kafka_settings.kafka_host', new="localhost"), \
          patch('api.config.kafka_settings.kafka_port', new=9092), \
-         patch('api.config.kafka_settings.kafka_connection', new=True):
+         patch('api.config.kafka_settings.kafka_connection', new=True), \
+         patch('api.config.kafka_settings.kafka_prefix', new="data_stream_"), \
+         patch('api.config.kafka_settings.max_streams', new=10):
 
         # Send GET request to the endpoint
         response = client.get("/status/kafka-details")
@@ -19,5 +21,7 @@ def test_kafka_details_route():
         assert response.json() == {
             "kafka_host": "localhost",
             "kafka_port": 9092,
-            "kafka_connection": True
+            "kafka_connection": True,
+            "kafka_prefix": "data_stream_",
+            "max_streams": 10
         }
