de0e22a92b7e0724fb5c0a9ec4473bf018f0a3be
Raul Bardaji <rbardaji@gmail.com>
2025-03-30 20:33:05 +0100

    feat: Add configuration flags for DXSpaces integration

--- api/config/swagger_settings.py
+++ api/config/swagger_settings.py
@@ -12,6 +12,10 @@ class Settings(BaseSettings):
     metrics_endpoint: str = "http://fed-api:80/metrics/"
     use_jupyterlab: bool = False
     jupyter_url: str = "https://jupyter.org/try-jupyter/lab/"
+    use_dxspaces: bool = False
+    dxspaces_url: str = "http://localhost:8001"
+    use_dataspaces: bool = False
+    dataspaces_url: str = "http://localhost:9000"
 
     model_config = {
         "env_file": "./env_variables/.env_swagger",
