5854d14452e8a9386f5b3916e761bfb525dc2354
Raul Bardaji <rbardaji@gmail.com>
2025-07-18 16:15:18 +0200

    chore: remove redirect router import from routes init
    Removes redirect_router import from api/routes/__init__.py for issue #95

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -1,6 +1,5 @@
 from .default_routes import router as default_router  # noqa: F401
 from .delete_routes import router as delete_router  # noqa: F401
-from .redirect_route import router as redirect_router  # noqa: F401
 from .register_routes import router as register_router  # noqa: F401
 from .search_routes import router as search_router  # noqa: F401
 from .status_routes import router as status_router  # noqa: F401
